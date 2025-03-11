667c8a775d7c35221fd18ad0a77e8096294a1ae4
siok <eladiohv2@gmail.com>
2025-03-10 20:38:38 -0500

    Prueba

---
+++ run.sh
@@ -0,0 +1,5 @@
+g++ src/main.cpp -o src/main
+echo 'Ejecucion de main.cpp'
+echo '######################################################\n'
+src/main | less
+echo '\n######################################################'
